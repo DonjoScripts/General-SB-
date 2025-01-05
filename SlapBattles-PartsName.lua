4e8af77003bd17a7bf875b65c9b7346754be714b
DonjoScripts <glomunki@gmail.com>
2025-01-05 07:06:04 +0700

    Update SlapBattles-PartsName.lua

--- SlapBattles-PartsName.lua
+++ SlapBattles-PartsName.lua
@@ -1,10 +1,7 @@
---local succ, err = pcall(function()
+
   local workspace = game:GetService("Workspace")
   local Lobby = workspace.Lobby
   local GloveStands = Lobby.GloveStands
   local RegularPortal = Lobby.Teleport1
   return Lobby, GloveStands, RegularPortal
---end)
-if err then
-  print(err)
-end
+
